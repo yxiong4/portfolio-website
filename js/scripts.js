c59e3da24c2c387bc6e5d5013f690fdd4f56a628
yxiong4 <monicaxiong12@gmail.com>
2023-02-16 15:13:49 -0500

    2.3 updated js
    collapsable menu bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
 console.log("Hello world")
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-}
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    x.className += ' responsive';
-}
-function mennuToggle() {
-    var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-    } else {
+        x.className += ' responsive';
+    } else { 
         x.className = 'navtoggle';
     }
 }
